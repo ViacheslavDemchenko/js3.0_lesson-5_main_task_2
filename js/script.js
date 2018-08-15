b3a977389f51ec3c7f8c73ef8c85c8558eed814d
Viacheslav Demchenko <demchenko_slava_@mail.ru>
2018-08-16 01:25:45 +0300

    Finished second main task of the fifth lesson

---
+++ js/script.js
@@ -0,0 +1,33 @@
+//По id
+let btnOpen = document.getElementById('open-btn'),//По id
+//По разным классам
+	nameValue = document.getElementsByClassName('name-value'),
+	budgetValue = document.getElementsByClassName('budget-value'),
+	goodsValue = document.getElementsByClassName('goods-value'),
+	itemsValue = document.getElementsByClassName('items-value'),
+	employersValue = document.getElementsByClassName('employers-value'),
+	discountValue = document.getElementsByClassName('discount-value'),
+	isopenValue = document.getElementsByClassName('isopen-value'),
+//По общему классу
+	goods_1 = document.getElementsByClassName('goods-item')[0],
+	goods_2 = document.getElementsByClassName('goods-item')[1],
+	goods_3 = document.getElementsByClassName('goods-item')[2],
+	goods_4 = document.getElementsByClassName('goods-item')[3],
+//По общему тегу
+	btnApprove = document.getElementsByTagName('button')[1],
+	btnCalc = document.getElementsByTagName('button')[2],
+	btnHire = document.getElementsByTagName('button')[3],
+//По разным селекторам
+	chooseItem = document.querySelector('.choose-item'),
+	timeValue = document.querySelector('.time-value'),
+	countBudgetItem = document.querySelector('.count-budget-value'),
+//По общему селектору
+	hireEmployersItem_1 = document.querySelectorAll('.hire-employers-item')[0],
+	hireEmployersItem_2 = document.querySelectorAll('.hire-employers-item')[1],
+	hireEmployersItem_3 = document.querySelectorAll('.hire-employers-item')[2];
+
+
+
+
+
+
